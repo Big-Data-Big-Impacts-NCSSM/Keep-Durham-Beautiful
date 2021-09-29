a1e8f0573a0c25e71088f34263a07834293b1820
KeshavVarad <kvarad2020@gmail.com>
2021-09-29 17:12:23 -0400

    Added endpoint to get litter hotspot data

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+var fs = require("fs");
+var parse = require("csv-parse");
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.send("Go to /getHotspots for data.");
+});
 
 app.get("/getHotspots", (req, res) => {
-  res.send("Hello World!");
+  var csvData = [];
+  fs.createReadStream(path.join(__dirname, "/litterHotspots.csv"))
+    .pipe(parse({ delimiter: "," }))
+    .on("data", function (csvrow) {
+      console.log(csvrow);
+      csvData.push(csvrow);
+    })
+    .on("end", function () {
+      res.send(csvData);
+    });
 });
 
 app.listen(port, () => {
