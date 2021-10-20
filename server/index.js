4555dbd7ac3b18e9c12b8f2f23c858afb164c2d0
KeshavVarad <kvarad2020@gmail.com>
2021-10-20 17:32:22 -0400

    Added geocoding

--- server/index.js
+++ server/index.js
@@ -1,57 +1,51 @@
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const express = require("express");
 const app = express();
 const port = 3000;
+var geocoder = require("geocoder");
 
-const {google} = require('googleapis');
-const keys = require('./keys.json');
+const { google } = require("googleapis");
+const keys = require("./keys.json");
 
-const client = new google.auth.JWT(
-    keys.client_email, null, key.private_key, ['https://www.googleapis.com/auth/spreadsheets']
-);
-
+const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [
+  "https://www.googleapis.com/auth/spreadsheets",
+]);
 
 //Creating the csvWriter
 const csvWriter = createCsvWriter({
-  path: '/litterHotspots.csv',
+  path: "/litterHotspots.csv",
   header: [
-    {id: 'Name', title: 'Name'},
-    {id: 'Address', title: 'Address'},
-    {id: 'Map', title: 'Map'},
-    {id: 'Description', title: 'Description'},
-    {id: 'Parking', title: 'Parking'},
-    {id: 'Types of Volunteers', title: 'Types of Volunteers'},
-    {id: 'Group Size', title: 'Group Size'},
-    {id: 'Type of Work', title: 'Type of Work'},
-    {id: 'Season', title: 'Season'},
-    {id: 'Other', title: 'Other'},
-    {id: 'major hotspot?', title: 'major hotspot?'},
-  ]
+    { id: "Name", title: "Name" },
+    { id: "Address", title: "Address" },
+    { id: "Map", title: "Map" },
+    { id: "Description", title: "Description" },
+    { id: "Parking", title: "Parking" },
+    { id: "Types of Volunteers", title: "Types of Volunteers" },
+    { id: "Group Size", title: "Group Size" },
+    { id: "Type of Work", title: "Type of Work" },
+    { id: "Season", title: "Season" },
+    { id: "Other", title: "Other" },
+    { id: "major hotspot?", title: "major hotspot?" },
+  ],
 });
 
-async function gsrun(cl){
+async function gsrun(cl) {
+  const gsapi = google.sheets({ version: "v4", auth: cl });
 
-    const gsapi = google.sheets({version:'v4', auth: cl});
+  const opt = {
+    spreadsheetId: "Add ID here",
+    range: "Data! (enter range here ex. A2:C6)",
+  };
 
-    const opt = {
-        spreadsheetId: "Add ID here"
-        range: 'Data! (enter range here ex. A2:C6)'
-    }
-
-    let data = await gsapi.spreadsheets.values.get(opt);
-    console.log(data.data.values);
-
+  let data = await gsapi.spreadsheets.values.get(opt);
+  console.log(data.data.values);
 }
 
 //Source: https://www.youtube.com/watch?v=MiPpQzW_ya0
 
-const data = [
-  {
-  }
-];
+const data = [{}];
 
-csvWriter
-  .writeRecords(data)
+csvWriter.writeRecords(data);
 
 //Source: https://www.codegrepper.com/code-examples/javascript/Writing+CSV+Files+nodejs
 
@@ -71,6 +65,15 @@ app.get("/getHotspots", (req, res) => {
   fs.createReadStream(path.join(__dirname, "/litterHotspots.csv"))
     .pipe(parse({ delimiter: "," }))
     .on("data", function (csvrow) {
+      if (csvrow[0] == "Name") {
+        csvrow.push("Longitude");
+        csvrow.push("Latitude");
+      } else {
+        geocoder.geocode(csvrow[1], function (err, data) {
+          console.log(data);
+        });
+      }
+
       console.log(csvrow);
       csvData.push(csvrow);
     })
